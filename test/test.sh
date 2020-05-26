0e7f35ee01154ecc4c09f5ea1615adf47879189a
chris <mail@chris007.de>
2020-05-26 21:02:13 +0200

    Using the classical cmake call

--- test/test.sh
+++ test/test.sh
@@ -5,9 +5,9 @@ parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
 
 mkdir -p build
-cmake -B ./build -S .
-cmake --build ./build
-cd ./build
+cd build
+cmake ..
+make
 GTEST_COLOR=1 ctest --output-on-failure .
 cd ../..
 gcovr -r . -e test_* -e arduino_mock* --html-details ./test/build/coverage.html --html-title ayab-test
