f9407f6db4b6f474506a8a7855bf373f66cc7f59
stla <stla@syntronic.com>
2020-05-27 10:34:13 +0200

    add beeper tests

---
+++ test/test_beeper.cpp
@@ -0,0 +1,42 @@
+#include "gtest/gtest.h"
+
+#include "../beeper.h"
+
+using ::testing::Return;
+
+class BeeperTest : public ::testing::Test {
+protected:
+  void SetUp() override {
+    arduinoMock = arduinoMockInstance();
+    b = Beeper();
+  }
+
+  void TearDown() override {
+    releaseArduinoMock();
+  }
+
+  void checkBeepTime(byte length) {
+    EXPECT_CALL(*arduinoMock, analogWrite(PIEZO_PIN, 0)).Times(length);
+    EXPECT_CALL(*arduinoMock, analogWrite(PIEZO_PIN, 20)).Times(length);
+    EXPECT_CALL(*arduinoMock, delay(BEEPDELAY)).Times(length * 2);
+    EXPECT_CALL(*arduinoMock, analogWrite(PIEZO_PIN, 255)).Times(1);
+  }
+
+  ArduinoMock *arduinoMock;
+  Beeper b;
+};
+
+TEST_F(BeeperTest, test_ready) {
+  checkBeepTime(5);
+  b.ready();
+}
+
+TEST_F(BeeperTest, test_finishedLine) {
+  checkBeepTime(3);
+  b.finishedLine();
+}
+
+TEST_F(BeeperTest, test_endWork) {
+  checkBeepTime(10);
+  b.endWork();
+}
