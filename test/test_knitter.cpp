3a42ded3075af820ce08c04ffa65396ce3c12681
stla <stla@syntronic.com>
2020-05-28 16:35:17 +0200

    Add knitter fsm tests

--- test/test_knitter.cpp
+++ test/test_knitter.cpp
@@ -6,6 +6,10 @@
 #include "knitter/solenoids_mock.h"
 
 using ::testing::Return;
+void onPacketReceived(const uint8_t *buffer, size_t size) {
+  (void)buffer;
+  (void)size;
+}
 
 class KnitterTest : public ::testing::Test {
 protected:
@@ -14,7 +18,10 @@ protected:
     beeperMock = beeperMockInstance();
     solenoidsMock = solenoidsMockInstance();
     encodersMock = encodersMockInstance();
-    k = Knitter(test_packetSerial);
+    serialMock = serialMockInstance();
+    // test_packetSerial->begin(SERIAL_BAUDRATE);
+    test_packetSerial.setPacketHandler(&onPacketReceived);
+    k = Knitter(&test_packetSerial);
   }
 
   void TearDown() override {
@@ -22,14 +29,16 @@ protected:
     releaseBeeperMock();
     releaseSolenoidsMock();
     releaseEncodersMock();
+    releaseSerialMock();
   }
 
   ArduinoMock *arduinoMock;
   BeeperMock *beeperMock;
   SolenoidsMock *solenoidsMock;
   EncodersMock *encodersMock;
+  SerialMock *serialMock;
   Knitter k;
-  SLIPPacketSerial *test_packetSerial;
+  SLIPPacketSerial test_packetSerial;
 };
 
 TEST(knitter_test, test_constructor) {
@@ -42,10 +51,27 @@ TEST_F(KnitterTest, test_constructor) {
 
 TEST_F(KnitterTest, test_isr) {
   EXPECT_CALL(*encodersMock, encA_interrupt).Times(1);
+  EXPECT_CALL(*encodersMock, getPosition).Times(1);
+  EXPECT_CALL(*encodersMock, getDirection).Times(1);
+  EXPECT_CALL(*encodersMock, getHallActive).Times(1);
+  EXPECT_CALL(*encodersMock, getBeltshift).Times(1);
+  EXPECT_CALL(*encodersMock, getCarriage).Times(1);
   k.isr();
 }
 
 TEST_F(KnitterTest, test_fsm) {
+  // Machine is initialized when left hall sensor is passed in Right direction
+  EXPECT_CALL(*encodersMock, encA_interrupt).Times(1);
+  EXPECT_CALL(*encodersMock, getPosition).Times(1);
+  EXPECT_CALL(*encodersMock, getDirection).Times(2).WillOnce(Return(Right));
+  EXPECT_CALL(*encodersMock, getHallActive).Times(1).WillOnce(Return(Left));
+  EXPECT_CALL(*encodersMock, getBeltshift).Times(1);
+  EXPECT_CALL(*encodersMock, getCarriage).Times(1);
+  k.isr();
+  EXPECT_CALL(*solenoidsMock, setSolenoids(0xFFFF)).Times(1);
+  // init to ready
+  k.fsm();
+  // ready
   k.fsm();
 }
 
