6058286ea9fc7930a8458bc1d26a334402413512
stla <stla@syntronic.com>
2020-06-03 11:21:51 +0200

    Add crc8 calculation

--- test/test_serial_encoding.cpp
+++ test/test_serial_encoding.cpp
@@ -23,8 +23,11 @@ TEST_F(SerialEncodingTest, test_testmsg) {
 }
 
 TEST_F(SerialEncodingTest, test_startmsg) {
-  uint8_t buffer[] = {reqStart_msgid};
+  uint8_t buffer[] = {reqStart_msgid, 0, 0, 0};
   onPacketReceived(buffer, sizeof(buffer));
+
+  // Not enough bytes
+  onPacketReceived(buffer, sizeof(buffer) - 1);
 }
 
 TEST_F(SerialEncodingTest, test_infomsg) {
@@ -33,38 +36,37 @@ TEST_F(SerialEncodingTest, test_infomsg) {
 }
 
 TEST_F(SerialEncodingTest, test_cnfmsg) {
-  uint8_t buffer[] = {cnfLine_msgid,
-                      0,
-                      0,
-                      0,
-                      0,
-                      0,
-                      0,
-                      0,
-                      0,
-                      0,
-                      0,
-                      0,
-                      0,
-                      0,
-                      0,
-                      0,
-                      0,
-                      0,
-                      0,
-                      0,
-                      0,
-                      0,
-                      0,
-                      0,
-                      0,
-                      0,
-                      0,
-                      1,
-                      0};
+  // CRC calculated with
+  // http://tomeko.net/online_tools/crc8.php?lang=en
+  constexpr uint8_t crc = 0xE9;
+
+  uint8_t buffer[29] = {cnfLine_msgid, // 0x42
+                        0x00,          0x00, 0xde, 0xad, 0xbe, 0xef, 0x00,
+                        0x00,          0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                        0x00,          0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                        0x00,          0x00, 0x00, 0x00, 0x00, 0x01, crc};
+
+  // Line not accepted
   onPacketReceived(buffer, sizeof(buffer));
+
+  // Line accepted, last line
   EXPECT_CALL(*knitterMock, setNextLine).WillOnce(Return(true));
   onPacketReceived(buffer, sizeof(buffer));
+
+  // Not last line
+  buffer[27] = 0x00;
+  buffer[28] = 0xB7;
+  EXPECT_CALL(*knitterMock, setNextLine).WillOnce(Return(true));
+  onPacketReceived(buffer, sizeof(buffer));
+
+  // crc wrong
+  EXPECT_CALL(*knitterMock, setNextLine).Times(0);
+  buffer[28]--;
+  onPacketReceived(buffer, sizeof(buffer));
+
+  // Not enough bytes in buffer
+  EXPECT_CALL(*knitterMock, setNextLine).Times(0);
+  onPacketReceived(buffer, sizeof(buffer) - 1);
 }
 
 TEST_F(SerialEncodingTest, test_debug) {
