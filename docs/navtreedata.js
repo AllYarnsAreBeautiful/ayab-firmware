8080491f5b7ea761b08737e880cff86402fffdf6
dl1com <1631996+dl1com@users.noreply.github.com>
2023-08-02 20:27:18 +0000

    Deploying to gh-pages from @ AllYarnsAreBeautiful/ayab-firmware@82d8b859ad6da57b334110c05833cd295555f966 🚀

---
+++ docs/navtreedata.js
@@ -0,0 +1,114 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "AYAB Arduino Firmware", "index.html", [
+    [ "PacketSerial", "index.html", [
+      [ "Description", "index.html#autotoc_md23", null ],
+      [ "Features", "index.html#autotoc_md24", null ],
+      [ "Background", "index.html#autotoc_md25", null ],
+      [ "Use", "index.html#autotoc_md26", [
+        [ "PacketSerial", "index.html#autotoc_md27", null ],
+        [ "Setup", "index.html#autotoc_md28", [
+          [ "Basic", "index.html#autotoc_md29", null ]
+        ] ],
+        [ "Advanced", "index.html#autotoc_md30", [
+          [ "Using A Non-Standard Serial Configuration", "index.html#autotoc_md31", null ],
+          [ "Using Secondary Serial Ports (e.g. Serial1, Serial2, etc)", "index.html#autotoc_md32", null ],
+          [ "Using SoftwareSerial", "index.html#autotoc_md33", null ],
+          [ "Other Streams", "index.html#autotoc_md34", null ]
+        ] ],
+        [ "Loop", "index.html#autotoc_md35", null ],
+        [ "Receiving Packets", "index.html#autotoc_md36", null ],
+        [ "Sending Packets", "index.html#autotoc_md37", null ]
+      ] ],
+      [ "Examples", "index.html#autotoc_md38", null ],
+      [ "Compatible Libraries", "index.html#autotoc_md39", null ],
+      [ "Changelog", "index.html#autotoc_md40", null ],
+      [ "License", "index.html#autotoc_md41", null ]
+    ] ],
+    [ "Changelog", "md__c_h_a_n_g_e_l_o_g.html", [
+      [ "1.0.0 / Unreleased", "md__c_h_a_n_g_e_l_o_g.html#autotoc_md1", null ],
+      [ "0.95 / 2018-12-06", "md__c_h_a_n_g_e_l_o_g.html#autotoc_md2", null ],
+      [ "0.90 / 2017-09-14", "md__c_h_a_n_g_e_l_o_g.html#autotoc_md3", null ],
+      [ "0.80 / 2017-04-16", "md__c_h_a_n_g_e_l_o_g.html#autotoc_md4", null ]
+    ] ],
+    [ "Contributing to AYAB", "md__c_o_n_t_r_i_b_u_t_i_n_g.html", null ],
+    [ "Carriage Hall Values", "md_doc_carriage_hallvalues.html", [
+      [ "KH910", "md_doc_carriage_hallvalues.html#autotoc_md7", null ],
+      [ "KH930", "md_doc_carriage_hallvalues.html#autotoc_md8", null ]
+    ] ],
+    [ "Finite State Machine", "md_doc_finite_state_machine.html", null ],
+    [ "Changelog", "md_lib__packet_serial__c_h_a_n_g_e_l_o_g.html", [
+      [ "[Unreleased]", "md_lib__packet_serial__c_h_a_n_g_e_l_o_g.html#autotoc_md11", [
+        [ "Removed", "md_lib__packet_serial__c_h_a_n_g_e_l_o_g.html#autotoc_md12", null ]
+      ] ],
+      [ "[1.2.0] 2017-11-09", "md_lib__packet_serial__c_h_a_n_g_e_l_o_g.html#autotoc_md13", [
+        [ "Added", "md_lib__packet_serial__c_h_a_n_g_e_l_o_g.html#autotoc_md14", null ],
+        [ "Removed", "md_lib__packet_serial__c_h_a_n_g_e_l_o_g.html#autotoc_md15", null ],
+        [ "Changed", "md_lib__packet_serial__c_h_a_n_g_e_l_o_g.html#autotoc_md16", null ]
+      ] ],
+      [ "[1.1.0] 2017-11-09", "md_lib__packet_serial__c_h_a_n_g_e_l_o_g.html#autotoc_md17", [
+        [ "Added", "md_lib__packet_serial__c_h_a_n_g_e_l_o_g.html#autotoc_md18", null ],
+        [ "Changed", "md_lib__packet_serial__c_h_a_n_g_e_l_o_g.html#autotoc_md19", null ],
+        [ "Removed", "md_lib__packet_serial__c_h_a_n_g_e_l_o_g.html#autotoc_md20", null ],
+        [ "Fixed", "md_lib__packet_serial__c_h_a_n_g_e_l_o_g.html#autotoc_md21", null ],
+        [ "Security", "md_lib__packet_serial__c_h_a_n_g_e_l_o_g.html#autotoc_md22", null ]
+      ] ]
+    ] ],
+    [ "LICENSE", "md_lib__packet_serial__l_i_c_e_n_s_e.html", null ],
+    [ "Deprecated List", "deprecated.html", null ],
+    [ "Todo List", "todo.html", null ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", null ],
+        [ "Functions", "functions_func.html", null ],
+        [ "Typedefs", "functions_type.html", null ]
+      ] ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "File List", "files.html", "files_dup" ],
+      [ "File Members", "globals.html", [
+        [ "All", "globals.html", null ],
+        [ "Functions", "globals_func.html", null ],
+        [ "Variables", "globals_vars.html", null ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"_adafruit___m_c_p23008_8h_source.html",
+"class_global_beeper.html#ab223b18df5a495327acb5840986b4b19",
+"class_tester_interface.html#a0e694eb1787cfe6ef5722508adfa27e8",
+"main_8cpp.html#ad1cee43a353537e5b6d7a44c4ae5b870"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
