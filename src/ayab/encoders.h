28e256343d0b7625efe98d3c516d91e4107a5b15
Tom Price <t0mpr1c3@gmail.com>
2023-09-10 17:08:48 -0400

    Update encoders.h

--- src/ayab/encoders.h
+++ src/ayab/encoders.h
@@ -168,8 +168,8 @@ private:
   volatile Carriage_t m_carriage;
   volatile Direction_t m_direction;
   volatile Direction_t m_hallActive;
-  volatile uint8_t m_position;
-  volatile unsigned char m_oldState; //atomic substitute for `bool` 
+  volatile unsigned char m_position; // atomic substitute for `uint8_t`
+  volatile unsigned char m_oldState; // atomic substitute for `bool` 
 
   void encA_rising();
   void encA_falling();
