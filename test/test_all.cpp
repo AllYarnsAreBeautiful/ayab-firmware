d846021aea4d403d7d2ba5550ca885c734d7f2fc
Tom Price <t0mpr1c3@gmail.com>
2023-09-11 01:10:09 -0400

    Update test_all.cpp

--- test/test_all.cpp
+++ test/test_all.cpp
@@ -55,8 +55,7 @@ TesterInterface *GlobalTester::m_instance = tester;
 
 int main(int argc, char *argv[]) {
   ::testing::InitGoogleMock(&argc, argv);
-	if (RUN_ALL_TESTS())
-	;
+	RUN_ALL_TESTS();
 	// Always return zero-code and allow PlatformIO to parse results
 	return 0;
 }
