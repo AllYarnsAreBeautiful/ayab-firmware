cd2d120d600d1190f2b6e7c2c7732291b8c79801
Tom Price <t0mpr1c3@gmail.com>
2023-09-11 02:41:29 -0400

    Update test_boards.cpp

--- test/test_boards.cpp
+++ test/test_boards.cpp
@@ -55,7 +55,5 @@ KnitterInterface *GlobalKnitter::m_instance = knitter;
 
 int main(int argc, char *argv[]) {
   ::testing::InitGoogleMock(&argc, argv);
-	if (RUN_ALL_TESTS()) {}
-	// Always return zero-code and allow PlatformIO to parse results
-	return 0;
+	return RUN_ALL_TESTS();
 }
