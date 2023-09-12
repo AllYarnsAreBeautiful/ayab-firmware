ca300bd9fbb3ad98d360fa72def759601e806725
Tom Price <t0mpr1c3@gmail.com>
2023-09-12 15:22:50 -0400

    Update encoders.h

--- src/ayab/encoders.h
+++ src/ayab/encoders.h
@@ -28,33 +28,33 @@
 
 // Enumerated constants
 
-enum class Direction : signed char { 
+enum class Direction : unsigned char { 
   NoDirection = -1, 
   Left = 0, 
   Right = 1
 };
-constexpr signed char NUM_DIRECTIONS = 2;
+constexpr int NUM_DIRECTIONS = 2;
 using Direction_t = enum Direction;
 
-enum class Carriage : signed char {
+enum class Carriage : unsigned char {
   NoCarriage = -1,
   Knit = 0,
   Lace = 1,
   Garter = 2
 };
-constexpr signed char NUM_CARRIAGES = 3;
+constexpr int NUM_CARRIAGES = 3;
 using Carriage_t = enum Carriage;
 
-enum class MachineType : signed char {
+enum class MachineType : unsigned char {
   NoMachine = -1,
   Kh910 = 0,
   Kh930 = 1,
   Kh270 = 2
 };
-constexpr signed char NUM_MACHINES = 3;
+constexpr int NUM_MACHINES = 3;
 using Machine_t = enum MachineType;
 
-enum class BeltShift : signed char { Unknown, Regular, Shifted, Lace_Regular, Lace_Shifted };
+enum class BeltShift : unsigned char { Unknown, Regular, Shifted, Lace_Regular, Lace_Shifted };
 using BeltShift_t = enum BeltShift;
 
 // Machine constants
