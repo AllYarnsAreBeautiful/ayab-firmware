ba29a37adf7850f3c4432e93941b308b5f1a5c69
Carson Holgate <carsonholgate@gmail.com>
2024-08-25 15:14:31 -0700

    Cleanup

--- src/ayab/encoders.h
+++ src/ayab/encoders.h
@@ -86,14 +86,14 @@ constexpr uint8_t START_OFFSET[NUM_MACHINES][NUM_DIRECTIONS][NUM_CARRIAGES] = {
     // KH910
     {
         // K,   L,   G
-        {40U, 32U, 32U/*16U*/}, // Left
-        {16U, 32U, 50U/*40U*/} // Right
+        {40U, 32U, 32U}, // Left
+        {16U, 32U, 50U} // Right
     },
     // KH930
     {
         // K,   L,   G
-        {40U, 40U, 32U}, // Left
-        {16U, 16U, 56U} // Right
+        {40U, 32U, 32U}, // Left
+        {16U, 32U, 50U} // Right
     },
     // KH270
     {
@@ -114,7 +114,6 @@ constexpr uint16_t FILTER_R_MAX[NUM_MACHINES] = {1023U, 600U, 600U};
 constexpr uint16_t SOLENOIDS_BITMASK = 0xFFFFU;
 
 constexpr uint8_t MAGNET_DISTANCE_270 = 12U;
-constexpr uint8_t HALF_MAGNET_DISTANCE_G = 12U;
 
 /*!
  * \brief Encoder interface.
