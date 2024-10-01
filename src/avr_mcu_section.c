5889f28b56eb8bad74601540bb98dcb99352277f
Jean-Philippe (JP) Cornil <jeanphilippe.cornil@gmail.com>
2024-10-01 11:12:57 +0200

    Add simavr support

---
+++ src/avr_mcu_section.c
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+#include "avr_mcu_section.h"
+
+extern void *__brkval;
+
+AVR_MCU (F_CPU, "atmega328p" );
+AVR_MCU_VOLTAGES(3300, 3300, 3300);
+AVR_MCU_VCD_FILE("ayab.vcd", 10000000);
+
+const struct avr_mmcu_vcd_trace_t _mytrace[]  _MMCU_ = {
+  { AVR_MCU_VCD_SYMBOL("v1"), .what = (void*) &PIND, .mask=(1<<PIND2), },
+  { AVR_MCU_VCD_SYMBOL("v2"), .what = (void*) &PIND, .mask=(1<<PIND3), },
+  { AVR_MCU_VCD_SYMBOL("BP"), .what = (void*) &PIND, .mask=(1<<PIND4), },
+  { AVR_MCU_VCD_SRAM_16("Stack"), .what = (void*) &SP, },
+  { AVR_MCU_VCD_SRAM_16("Heap"), .what = (void*)(&__brkval), },
+};
